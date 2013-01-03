f11667b2ea45dd77bc008b9da291f4ad6077daed
Gampol Thitinilnithi <gampolt@gmail.com>
2013-01-03 11:07:02 +0700

    Model.js: Party(allow, deny), RSVP(rsvp)

--- model.js
+++ model.js
@@ -10,13 +10,24 @@
 Parties = new Meteor.Collection("parties");
 
 Parties.allow({
-  insert: function(){},
-  update: function(){},
-  remove: function(){}
+  insert: function(userId, doc){
+    return false; // use createParty method instead
+  },
+  update: function(userId, docs, fields, modifier){
+    return true; // XXX
+  },
+  remove: function(userId, docs){
+    return true;
+  }
 });
 
 Parties.deny({
-  remove: function(){}
+  remove: function(userId, parties){
+    return _.any(parties, function(party){
+      // Can't delete a party with RSVP's
+      return party.attending > 0;
+    })
+  }
 });
 
 /*
@@ -28,6 +39,34 @@ Parties.deny({
 Rsvps = new Meteor.Collection("rsvps");
 
 Meteor.methods({
-  createParty:function(){},
-  rsvp: function(){}
+  // title, description, x, y, public
+  createParty:function(options){
+    options = options || {};
+    if (! (typeof options.title === "string" && options.title.length && typeof options.description === "string" && options.description.length && typeof options.x >=0 && options.x <= 1 && options.y >= 0 && options.y <= 1 ))
+      throw new Meteor.Error(400, "Required parameter missing");
+    if (! this.userId)
+      throw new Meteor.Error(403, "You must be logged in")
+  },
+  
+  rsvp: function(partyId, rsvp){
+    if (! this.userID)
+      throw new Meteor.Error(403, "You must be logged in to RSVP");
+    if (! _.contains(['yes', 'no', 'maybe'], rsvp))
+      throw new Meteor.Error(400, "Invalid RSVP");
+    
+    var oldAttendance;
+    var attendance = (rsvp === "yes") ? 1 : 0;
+    
+    var record = Rsvps.findOne({user: this.userId, party: partyId});
+    if (record){
+      var oldAttendance = (record.rsvp === 'yes') ? 1 : 0;
+      Rsvps.insert({user: this.userId, party: partyId, rsvp: rsvp});
+    }
+    
+    Parties.update(partyId, {$inc: {attending: attendance - oldAttendance}});
+    
+    if (! record){
+      record = {user: this.userId, party: partyId}
+    }
+  }    
 });
